eb49039a520edb32841d36d4b08df00ebda2158f
kirthi-javadev <57347151+kirthi-javadev@users.noreply.github.com>
2019-11-06 09:56:25 +0530

    Add files via upload

---
+++ Forloop1.java
@@ -0,0 +1,12 @@
+package javafundamentals;
+
+public class Forloop1 {
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			for(int i=0;i<5;i++)
+			{
+				System.out.println("Hello Java... " + i);
+				System.out.println("I have done...! ");
+			}
+		}	
+	}
