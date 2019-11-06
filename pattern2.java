bf64f27d5461696fc20be874654325e6d972ff93
kirthi-javadev <57347151+kirthi-javadev@users.noreply.github.com>
2019-11-06 10:06:39 +0530

    Add files via upload

---
+++ pattern2.java
@@ -0,0 +1,23 @@
+package javafundamentals;
+
+public class pattern2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int k=8;
+		for (int i=0;i<5;i++)
+		{
+			for (int j=0;j<k;j++)
+			{
+				System.out.print(" ");
+			}
+			k=k-2;
+			for (int j=0;j<=i;j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println( );
+		}
+	}
+}
+
